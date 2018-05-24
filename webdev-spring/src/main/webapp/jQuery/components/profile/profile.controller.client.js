a44f3512d7888ef2117529ee0cca5fd54810c006
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-05-24 01:18:07 -0700

    added profile controller with implementation

---
+++ webdev-spring/src/main/webapp/jQuery/components/profile/profile.controller.client.js
@@ -0,0 +1,47 @@
+(function () {
+    var $phone, $contact, $email, $role, $dob, $username;
+    var $logoutBtn, $updateBtn;
+    var userService = new UserServiceClient();
+    $(main);
+
+    function main() {
+    	var userId = window.location.href.split('=')[1];
+    	userService.findUserById(userId).then(showProfile);
+    	$updateBtn =$('#wbdv-update');
+    	$updateBtn.click(updateProfile);	
+    	$logoutBtn =$('#wbdv-logout');
+    	$logoutBtn.click(logout);		
+    }
+
+    function showProfile(user){
+        $('#wbdv-username').val(user.username);
+        $('#wbdv-phone').val(user.phone);
+        $('#wbdv-email').val(user.email);
+        $('#wbdv-role').val(user.role);
+        $('#wbdv-dob').val();
+
+    }
+    function logout() {
+    	window.location.href='../login/login.template.client.html';
+        }
+
+
+    function updateProfile(user){
+        $username = $('#wbdv-username').val();
+    	$contact = $('#wbdv-phone').val();
+    	$email = $('#wbdv-email').val();
+        $role = $('#wbdv-role').val();
+        $dob = $('#wbdv-dob').val();
+
+        var newUser = new User(
+            $username,
+            null, null, null,
+            $role,
+            $contact,
+            $email,
+            $dob);
+
+        userService.updateProfile(newUser)
+            .then(showStatus);
+    }
+})();
