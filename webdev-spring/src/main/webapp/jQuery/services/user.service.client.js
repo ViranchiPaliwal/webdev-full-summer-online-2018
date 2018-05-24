d5f10c613bcd65e675e7f0a038774d3efa8ffaad
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-05-23 17:10:51 -0700

    adding register html template

--- webdev-spring/src/main/webapp/jQuery/services/user.service.client.js
+++ webdev-spring/src/main/webapp/jQuery/services/user.service.client.js
@@ -1,48 +1,48 @@
 function UserServiceClient() {
-    this.createUser = createUser;
-    this.findAllUsers = findAllUsers;
-    this.findUserById = findUserById;
-    this.deleteUser = deleteUser;
-    this.updateUser = updateUser;
-    this.url = 'http://localhost:8080/api/user';
-    var self = this;
-    function createUser(newUser, callback) {
-        return fetch(self.url,{
-            method: 'post',
-            body: JSON.stringify(newUser),
-            headers:{
-                'content-type':'application/json'
-            }
-        });
-    }
-    function findAllUsers(callback) {
-        return fetch(self.url)
-               .then(function(response){
-                   return response.json();
-               });
-    }
+	this.createUser = createUser;
+	this.findAllUsers = findAllUsers;
+	this.findUserById = findUserById;
+	this.deleteUser = deleteUser;
+	this.updateUser = updateUser;
+	this.url = 'http://localhost:8080/api/user';
+	var self = this;
+	function createUser(newUser, callback) {
+		return fetch(self.url,{
+			method: 'post',
+			body: JSON.stringify(newUser),
+			headers:{
+				'content-type':'application/json'
+			}
+		});
+	}
+	function findAllUsers(callback) {
+		return fetch(self.url)
+		.then(function(response){
+			return response.json();
+		});
+	}
 
-    function findUserById(userId, callback){
-        return fetch(self.url+'/'+userId)
-            .then(function(response){
-                return response.json();
-            });
-    }
-    function updateUser(userId, updatedUser, callback){
-        return fetch(self.url+'/'+userId,{
-            method: 'put',
-            body: JSON.stringify(updatedUser),
-            headers:{
-                'content-type':'application/json'
-            }
-        })
-            .then(function(response){
-                return response.json();
-            });
-    }
-    function deleteUser(userId, callback) {
-        return fetch(self.url+'/'+userId,{
-            method: 'delete'
-        });
-    }
+	function findUserById(userId, callback){
+		return fetch(self.url+'/'+userId)
+		.then(function(response){
+			return response.json();
+		});
+	}
+	function updateUser(userId, updatedUser, callback){
+		return fetch(self.url+'/'+userId,{
+			method: 'put',
+			body: JSON.stringify(updatedUser),
+			headers:{
+				'content-type':'application/json'
+			}
+		})
+		.then(function(response){
+			return response.json();
+		});
+	}
+	function deleteUser(userId, callback) {
+		return fetch(self.url+'/'+userId,{
+			method: 'delete'
+		});
+	}
 }
