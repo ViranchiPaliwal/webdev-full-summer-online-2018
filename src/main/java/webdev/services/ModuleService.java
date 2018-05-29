206deb8d3cfccb3722519a9c92167d5be6a4b8af
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-05-29 07:35:21 -0700

    added modules service

---
+++ src/main/java/webdev/services/ModuleService.java
@@ -0,0 +1,24 @@
+package webdev.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import webdev.models.Course;
+import webdev.repositories.CourseRepository;
+
+@CrossOrigin(origins="*") 
+@RestController
+public class ModuleService {
+	@Autowired
+	CourseRepository courseRepository;	
+	@GetMapping("/api/course")
+	public Iterable<Course> findAllCourses() {
+		return courseRepository.findAll(); 
+	}
+	
+	@PostMapping("/api/course")
+	public Course createCourse
+	(@RequestBody Course course) {
+			return courseRepository.save(course);
+	}
+}
