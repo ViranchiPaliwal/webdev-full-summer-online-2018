e2da0747d76ca46aa27dd75a8abb961d07b88206
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-05-29 20:32:25 -0700

    implemented findAllModulesForCourse service functionality

--- src/main/java/webdev/services/ModuleService.java
+++ src/main/java/webdev/services/ModuleService.java
@@ -1,24 +1,48 @@
 package webdev.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import webdev.models.Module;
 import webdev.models.Course;
+
+import webdev.repositories.ModuleRepository;
 import webdev.repositories.CourseRepository;
 
-@CrossOrigin(origins="*") 
+
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ModuleService {
 	@Autowired
-	CourseRepository courseRepository;	
-	@GetMapping("/api/course")
-	public Iterable<Course> findAllCourses() {
-		return courseRepository.findAll(); 
+	CourseRepository courseRepository;
+	@Autowired
+	ModuleRepository moduleRepository;
+	@PostMapping("/api/course/{courseId}/module")
+	public Module createModule(@PathVariable("courseId") int courseId,
+			@RequestBody Module newModule) {
+		Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			Course course = data.get();
+			newModule.setCourse(course);
+			return moduleRepository.save(newModule);
+		}
+		return null;
 	}
 	
-	@PostMapping("/api/course")
-	public Course createCourse
-	(@RequestBody Course course) {
-			return courseRepository.save(course);
+	@GetMapping("/api/course/{courseId}/module")
+	public List<Module> findAllModulesForCourse(
+			@PathVariable("courseId") int courseId) {
+		Optional<Course> data =
+	courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			Course course = data.get();
+			return course.getModules();
+		}
+		return null;		
 	}
+
+
 }
