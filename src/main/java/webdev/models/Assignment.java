3f9a5e87af19ba9243cef24dc2c3c73ab813f007
vpaliwal <viranchi.paliwal@gmail.com>
2018-07-01 01:50:36 -0700

    Assignment model implementation

---
+++ src/main/java/webdev/models/Assignment.java
@@ -0,0 +1,28 @@
+package webdev.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Assignment extends Question {
+	private String title;
+	private String description;
+	private int points;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+}
