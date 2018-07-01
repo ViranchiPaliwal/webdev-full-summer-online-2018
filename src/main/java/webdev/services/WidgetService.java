ad7205bdd3eb1c0a64391815b3411cd739e9a496
vpaliwal <viranchi.paliwal@gmail.com>
2018-06-30 22:15:37 -0700

    react native WidgetService

--- src/main/java/webdev/services/WidgetService.java
+++ src/main/java/webdev/services/WidgetService.java
@@ -2,7 +2,6 @@ package webdev.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,85 +13,34 @@ import webdev.repositories.WidgetRepository;
 import webdev.repositories.TopicRepository;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*")
 public class WidgetService {
 	@Autowired
-	WidgetRepository widgetRepository;
-
+	WidgetRepository repository;
 	@Autowired
-	private TopicRepository topicRepository;
-
+	TopicRepository topicRepository;
+	
+	@GetMapping("/api/topic/{topicId}/widget")
+	public List<Widget> findAllWidgetsForTopic(@PathVariable("topicId") int topicId) {
+		Optional<Topic> optionalTopic = topicRepository.findById(topicId);
+		if(optionalTopic.isPresent()) {
+			Topic topic = optionalTopic.get();
+			return topic.getWidgets();
+		}
+		return null;
+	}
+	
+	@PostMapping("/api/widget/save")
+	public void saveAllWidgets(@RequestBody
+			List<Widget> widgets) {
+		repository.deleteAll();
+		for(Widget widget: widgets) {
+			repository.save(widget);
+		}
+	}
+	
 	@GetMapping("/api/widget")
 	public List<Widget> findAllWidgets() {
-		return (List<Widget>) widgetRepository.findAll();
-	}
-
-	@GetMapping("/api/widget/{widgetId}")
-	public Widget getWidgetById(@PathVariable("widgetId") int widgetId) {
-		Optional<Widget> widget = widgetRepository.findById(widgetId);
-		if (widget.isPresent()) {
-			return widget.get();
-		}
-		return null;
-	}
-
-	@GetMapping("api/topic/{topicId}/widget")
-	public List<Widget> getWidgetsByTopicId(@PathVariable("topicId") int topicId) {
-		Optional<Topic> topic = topicRepository.findById(topicId);
-		if (topic.isPresent()) {
-			List<Widget> widgets = topic.get().getWidgets();
-			Collections.sort(widgets);
-			return widgets;
-		}
-		return null;
-	}
-
-	@PostMapping("api/topic/{topicId}/widget")
-	public void saveAllWidgets(@PathVariable("topicId") int topicId, @RequestBody List<Widget> widgets) {
-		Optional<Topic> topic = topicRepository.findById(topicId);
-		if (topic.isPresent()) {
-			List<Widget> oldWidgetlist = topic.get().getWidgets();
-			for (Widget oldWidget : oldWidgetlist) {
-				widgetRepository.deleteById(oldWidget.getId());
-			}
-			for (Widget newWidget : widgets) {
-				newWidget.setTopic(topic.get());
-				widgetRepository.save(newWidget);
-			}
-		}
-	}
-
-	@PutMapping("/api/widget/{widgetId}")
-	public Widget updateWidget(@PathVariable("widgetId") int widgetId, @RequestBody Widget newWidget) {
-		Optional<Widget> data = widgetRepository.findById(widgetId);
-		if (data.isPresent()) {
-			Widget widget = data.get();
-			widget.setText(newWidget.getText());
-			widget.setListType(newWidget.getListType());
-			widget.setLinkUrl(newWidget.getLinkUrl());
-			widget.setWidgetType(newWidget.getWidgetType());
-			widget.setImageLink(newWidget.getImageLink());
-			widget.setWidgetName(newWidget.getWidgetName());
-			widget.setListContent(newWidget.getListContent());
-			widget.setText(newWidget.getText());
-			widget.setTopic(newWidget.getTopic());
-			widget.setLinkContent(newWidget.getLinkContent());
-			widget.setWidgetPos(newWidget.getWidgetPos());
-			return widget;
-		}
-		return null;
-	}
-
-	@PostMapping("api/widget/save")
-	public void saveAllWidgets(@RequestBody List<Widget> widgets) {
-		widgetRepository.deleteAll();
-		for (Widget widget : widgets) {
-			widgetRepository.save(widget);
-		}
-	}
-
-	@DeleteMapping("/api/widget/{widgetId}")
-	public void deleteWidget(@PathVariable("widgetId") int widgetId) {
-		widgetRepository.deleteById(widgetId);
+		return (List<Widget>) repository.findAll();
 	}
 }
