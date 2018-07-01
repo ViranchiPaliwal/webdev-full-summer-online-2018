455b737c0248b5a3b8c7ea85bbec8d93eaad1248
vpaliwal <viranchi.paliwal@gmail.com>
2018-06-30 18:20:23 -0700

    react native Widget model

--- src/main/java/webdev/models/Widget.java
+++ src/main/java/webdev/models/Widget.java
@@ -1,90 +1,31 @@
 package webdev.models;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class Widget implements Comparable<Widget>{
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Widget {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String text;
 	private String widgetType;
-	private int size;
-	private String widgetName;
-	private String imageLink;
-	private String listContent;
-	private String listType;
-	private String linkContent;
-	private String linkUrl;
-	private int widgetPos; 
 	@ManyToOne
 	@JsonIgnore
 	private Topic topic;
-	
-	public int getWidgetPos() {
-		return widgetPos;
+	public String getWidgetType() {
+		return widgetType;
 	}
-	public void setWidgetPos(int widgetPos) {
-		this.widgetPos = widgetPos;
-	}
-	public void setSize(int size) {
-		this.size = size;
-	}
-	
-	public String getWidgetName() {
-		return widgetName;
-	}
-	public void setWidgetName(String widgetName) {
-		this.widgetName = widgetName;
-	}
-	public String getImageLink() {
-		return imageLink;
-	}
-	public void setImageLink(String imageLink) {
-		this.imageLink = imageLink;
-	}
-	public String getListContent() {
-		return listContent;
-	}
-	public void setListContent(String listContent) {
-		this.listContent = listContent;
-	}
-	public String getListType() {
-		return listType;
-	}
-	public void setListType(String listType) {
-		this.listType = listType;
-	}
-	public String getLinkContent() {
-		return linkContent;
-	}
-	public void setLinkContent(String linkContent) {
-		this.linkContent = linkContent;
-	}
-	public String getLinkUrl() {
-		return linkUrl;
-	}
-	public void setLinkUrl(String linkUrl) {
-		this.linkUrl = linkUrl;
-	}
-	public String getParaContent() {
-		return paraContent;
-	}
-	public void setParaContent(String paraContent) {
-		this.paraContent = paraContent;
-	}
-	private String paraContent;
-	
-	public Topic getTopic() {
-		return topic;
-	}
-	public void setTopic(Topic topic) {
-		this.topic = topic;
-	}
-	
-	public int getSize() {
-		return size;
+	public void setWidgetType(String widgetType) {
+		this.widgetType = widgetType;
 	}
 	public int getId() {
 		return id;
@@ -98,16 +39,10 @@ public class Widget implements Comparable<Widget>{
 	public void setText(String text) {
 		this.text = text;
 	}
-	public String getWidgetType() {
-		return widgetType;
+	public Topic getTopic() {
+		return topic;
 	}
-	public void setWidgetType(String widgetType) {
-		this.widgetType = widgetType;
-	}
-	
-	@Override
-	public int compareTo(Widget o) {
-		return this.getWidgetPos()-o.getWidgetPos();
+	public void setTopic(Topic topic) {
+		this.topic = topic;
 	}
 }
-
