0fe575966158126ab34b957ac0b8576e271a304f
vpaliwal <viranchi.paliwal@gmail.com>
2018-06-30 22:59:27 -0700

    FillInTheBlankQuestion repository

---
+++ src/main/java/webdev/repositories/FillInTheBlankQuestionRepository.java
@@ -0,0 +1,7 @@
+package webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import webdev.models.FillInTheBlankQuestion;
+
+public interface FillInTheBlankQuestionRepository
+extends CrudRepository<FillInTheBlankQuestion, Integer> { }
