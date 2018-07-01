ffdf6e58372711a1a8285ed6a96b9b49842fd71e
vpaliwal <viranchi.paliwal@gmail.com>
2018-06-30 22:57:01 -0700

    EssayQuestion repository

---
+++ src/main/java/webdev/repositories/EssayQuestionRepository.java
@@ -0,0 +1,7 @@
+package webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import webdev.models.EssayQuestion;
+
+public interface EssayQuestionRepository
+extends CrudRepository<EssayQuestion, Integer> { }
